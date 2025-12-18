9ef254cc0f2439661b505577e606e39851d0ffcd
Andre199 <130306826+Andre199tw@users.noreply.github.com>
2025-12-18 17:15:16 +0100

    Create serverUDP.c

---
+++ server-UDP_G14/serverUDP.c
@@ -0,0 +1,23 @@
+/*
+ * serverUDP.c
+ *
+ *  Created on: 17 dic 2025
+ *      Author: Andrea Martilotti
+ *      Author: Michele Ritella
+ */
+
+#include <stdio.h>
+#include <winsock.h>
+
+
+int main(){
+
+	WSADATA wsaData;
+
+
+
+
+
+
+	return 0;
+}
